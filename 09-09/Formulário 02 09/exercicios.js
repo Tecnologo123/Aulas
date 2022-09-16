f394386344718d9ce5471332608362c1ce31e94c
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-09-16 13:56:18 -0300

    exercicios

---
+++ 09-09/Formulário 02 09/exercicios.js
@@ -0,0 +1,25 @@
+function clicou() {
+    window.alert('Informações salvas com sucesso!')
+}
+function clicou2() {
+    window.alert('Informações excluídas com sucesso!')
+}
+function clicou3() {
+    var nome = "Branco"
+
+    if (nome == "Preenchido") {
+        console.log("Nome preenchido com sucesso");
+    } else {
+        console.log("O campo nome deve ser preenchido");
+    }
+  
+}
+function clicou4() {
+    var nome = "Preenchido"
+
+    if (nome == "Branco") {
+        console.log("Informações não salvas");
+    } else {
+        console.log("Informações salvas com sucesso")
+    }
+}
