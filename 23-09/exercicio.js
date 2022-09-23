676914d3de583b29c1ee94b9324ca50e190fd5f8
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-09-23 17:31:10 -0300

    exercicio em andamento

--- 23-09/exercicio.js
+++ 23-09/exercicio.js
@@ -15,11 +15,11 @@ function calcular() {
 
 
     if (n1 > n2) {
-        par1.innerHTML = `O primeiro número é ${n1} e o segundo número é ${n2}. ${n1} é maior que ${n2}`
+        window.alert(`O primeiro número é ${n1} e o segundo número é ${n2}. ${n1} é maior que ${n2}`)
     } else if (n2 > n1) {
-        par1.innerHTML = `O segundo número é ${n2} e o primeiro número é ${n1}. ${n2} é maior que ${n1}`
+        window.alert(`O segundo número é ${n2} e o primeiro número é ${n1}. ${n2} é maior que ${n1}`)
     } else {
-        par1.innerHTML = (`Os dois números são iguais`)
+        window.alert(`Os dois números são iguais`)
     }
 
 }
@@ -44,7 +44,54 @@ function calcular2() {
         case 4 :
             resultado = n1 / n2
             window.alert(`O resultado da divisão é ${resultado}`)
+            break
         default :
             window.alert(`Opção inválida`)
+            break
+    }
+}
+function calcular_media2() {
+    var nome = window.prompt('Solicitar nome');
+    var nota1 = Number(window.prompt('Solicitar nota1:'));
+    var nota2 = Number(window.prompt('Solicitar nota2:'));
+    var media = (nota1 + nota2)/2
+
+    if (media <=7) {
+        window.alert(`${nome}, estude mais!`)
+    }else {
+        window.alert(`Parabéns, ${nome}!`)
+    }
+}
+function par_impar(){
+    var num = Number(window.prompt('Insira um número'))
+    if (num %2 == 0) {
+        window.alert('Par!')
+    }else {
+        window.alert('Impar!')
+    } 
+
+}
+function calcular_idade() {
+    var anonsc = (window.prompt('Insira o ano de nascimento'))   
+}
+function opcoes() {
+    var opcao = Number(window.prompt('Escolha uma opção de operação matemática: \n 1)Calcular idade\n 2)Calcular média \n 3)Número maior \n 4)Par ou Impar ')); 
+
+    switch (opcao) {
+        case 1 :
+            
+            break
+        case 2 :
+            calcular_media2() 
+            break
+        case 3 :
+            calcular()
+            break
+        case 4 :
+            par_impar()
+            break
+        default:
+            window.alert(`Opção inválida`)
+            break
     }
 }
