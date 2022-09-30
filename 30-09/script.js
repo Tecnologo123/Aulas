618f0a4d51906e3d021598428390a74b8f33e5aa
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-09-30 16:17:24 -0300

    exercício 3 pronto

--- 30-09/script.js
+++ 30-09/script.js
@@ -32,3 +32,19 @@ function calcular_media2() {
     var media = soma/qtd
     window.alert(`A média da turma é ${media}`)    
 }
+function calcular_par_impar() {
+    var mensagem_inteira = ""
+    var mensagem
+    
+    for(var i = 0 ;i <=10 ;i++) {
+        if(i % 2 == 0) {
+            mensagem = `${i} é par!\n`
+            mensagem_inteira += mensagem
+        } else {
+            mensagem=`${i} é ímpar\n`
+            mensagem_inteira += mensagem
+        }
+        alert(mensagem_inteira)
+    }
+    
+}
