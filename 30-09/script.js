eef1f170f2d0609ab5ac9a4ee5c952773bc148b1
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-09-30 14:54:06 -0300

    exercicio feito

--- 30-09/script.js
+++ 30-09/script.js
@@ -1,11 +1,11 @@
 function par_impar(){
-    elemento = document.getElementById('paragrafo');
-    var num = Number(window.prompt('Insira um número máximo'))
-    message = "TESTE"
-    if (num %2 == 0) {
-        message += "teste"
-    }else {
-        message += "teste"
-    } 
-    elemento.innerHTML = message
+    var soma = 0
+
+    for (var i = 0 ;i < 10 ;i++) {
+        var nota = Number(window.prompt('Digite uma nota:'))
+        soma = soma+nota
+    }
+    var media = soma/10
+    window.alert(`A média da turma é ${media}`) 
+
 }
