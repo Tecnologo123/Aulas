44dabd64c7111523687e2800a7017e8d88851225
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-09-30 15:11:32 -0300

    exercício 3 pronto

--- 30-09/script.js
+++ 30-09/script.js
@@ -9,3 +9,14 @@ function par_impar(){
     window.alert(`A média da turma é ${media}`) 
 
 }
+function calcular_media() {
+    var qtd = window.prompt('Quantas notas deseja inserir ?') 
+    var soma = 0
+
+    for (var i = 0 ;i < qtd ;i++) {
+        var nota = Number(window.prompt('Digite uma nota:'))
+        soma = soma+nota
+    }
+    var media = soma/qtd
+    window.alert(`A média da turma é ${media}`)    
+}
