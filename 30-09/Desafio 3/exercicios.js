785b8df44fae275abd9a9e63ed48912049ca3f3c
marisangila <marisangila.alves@edu.sc.senai.br>
2022-10-07 14:15:09 -0300

    Update exercicios.js

--- 30-09/Desafio 3/exercicios.js
+++ 30-09/Desafio 3/exercicios.js
@@ -1,20 +1,24 @@
 function calcular() {
-    var maior 
+    var maior = 0
     for (var i = 0; i < 5; i ++) {
         var numero = Number(window.prompt('Digite um número:'))
         if (numero > maior)
-            maior = numero;
+            maior = numero
     } 
-    alert(`${numero} é o maior número`)
+    alert(`${maior} é o maior número`)
 }
 
 function tabuada() {
-    var num = Number(window.prompt('Digite o número de qual tabuada deseja realizar:'))
-    for (var i = 1; i <= 10, i++) {
-        multiplicacao += num
+    mensagem_inteira = ""
+    mensagem = ""
+    var tab = 0
+    var num = Number(prompt('Digite o número de qual tabuada deseja realizar:'))
+    for (var i = 1; i <= 10; i++) {
+        tab = num * i
+        mensagem = `${num} x ${i} = ${tab}\n`
+        mensagem_inteira += mensagem
     }
-    var tabuada = num * i
-    window.alert(`A tabuada do número digitado é ${tabuada}`) 
+    alert(mensagem_inteira) 
 }
 
 
