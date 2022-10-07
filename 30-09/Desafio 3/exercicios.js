7acd0ae21cfc0d1ec016ae170a757dde120a017f
FelipeVoigt <felipe_voigt1@estudante.sesisenai.org.br>
2022-10-07 13:57:27 -0300

    Desafio 3 Tarefa

---
+++ 30-09/Desafio 3/exercicios.js
@@ -0,0 +1,24 @@
+function calcular() {
+    var maior 
+    for (var i = 0; i < 5; i ++) {
+        var numero = Number(window.prompt('Digite um número:'))
+        if (numero > maior)
+            maior = numero;
+    } 
+    alert(`${numero} é o maior número`)
+}
+
+function tabuada() {
+    var num = Number(window.prompt('Digite o número de qual tabuada deseja realizar:'))
+    for (var i = 1; i <= 10, i++) {
+        multiplicacao += num
+    }
+    var tabuada = num * i
+    window.alert(`A tabuada do número digitado é ${tabuada}`) 
+}
+
+
+
+
+
+
