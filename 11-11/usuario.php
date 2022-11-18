db0cf24436729517e5da2fe5a518b0374a6766a4
FelipeVoigt <101840661+Tecnologo123@users.noreply.github.com>
2022-11-18 14:45:26 -0300

    inserir campos email

---
+++ 11-11/usuario.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container text-center">
+                  <div class="row">
+                    <div class="col">
+                      <img class="aula_css2"src="logo.svg" width="100" height="60">
+                      <br>
+                    </div class="container text-center">
+                  </br>
+                
+                    <p><h3>Faça login</h3></p>
+                  
+                    <form>
+                        <div class="form-group">
+                          <input style="width: 300px" class="aula_css" type="seuemail" name="email" class="form-control" id="exampleInputSeuemail1" aria-describedby="emailHelp" placeholder="Seu email">
+                        </div>
+                        <div class="form-group">
+                          <input style="width: 300px" class="aula_css" type="senha" name="senha" class="form-control" id="exampleInputSenha" placeholder="Senha">
+                        </div>
+                        </div>
+                        <div class="form-check">
+                          <input class="aula_css2" class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                          <label class="form-check-label" for="flexCheckDefault">
+                            Lembrar de mim
+                          </label>
+                        </div>
+                        <br><p><button style="width: 300px" type="button" class="btn btn-primary btn-lg">Login</button></p></br>
+                      </form> 
+                    </div>
+                  </div>
+                </div>
+      
+    
+</body>
+</html>
+    
+</body>
+</html>
